6b716e10a5eeda77faedb0f913e3b189eab520d0
K-33-name <kpmparambath@gmail.com>
2026-03-22 18:45:06 +0530

    fixed secret key config

--- config.py
+++ config.py
@@ -1,19 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = "set_secret"
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
 
-    if os.getenv("MYSQLHOST"):  
-        # 🚀 Railway (production)
-        DB_HOST = os.getenv("MYSQLHOST")
-        DB_USER = os.getenv("MYSQLUSER")
-        DB_PASSWORD = os.getenv("MYSQLPASSWORD")
-        DB_NAME = os.getenv("MYSQLDATABASE")
-        DB_PORT = int(os.getenv("MYSQLPORT", 3306))
-    else:
-        # 💻 Local (XAMPP)
-        DB_HOST = "localhost"
-        DB_USER = "root"
-        DB_PASSWORD = ""   # default XAMPP
-        DB_NAME = "setdb"
-        DB_PORT = 3306
+    DB_HOST = os.getenv("MYSQLHOST")
+    DB_USER = os.getenv("MYSQLUSER")
+    DB_PASSWORD = os.getenv("MYSQLPASSWORD")
+    DB_NAME = os.getenv("MYSQLDATABASE")
+    DB_PORT = int(os.getenv("MYSQLPORT", 3306))
